b485d2c00734788db59cc536c84a21cd90e644da
SilverOracle <sanemax15@gmail.com>
2019-06-07 17:35:35 +0300

    Modifying API

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 const path = require('path');
+const config = require('config');
 const fs = require('fs');
 const gm = require('gm');
 
-const users = require('./routes/api/Users');
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
 
 const app = express();
 // Add path to images
 app.use("/public", express.static(path.join(__dirname, 'public')));
+
+//app.use(express.static(path.join(__dirname + 'views')));
 
 app.set('view engine', 'pug');
 
@@ -17,46 +22,52 @@ app.set('view engine', 'pug');
 app.get('/', function (req, res) { 
     res.render('index.pug');
 });
-
 app.get('/views/home.pug', function (req, res) { 
     res.render('home.pug');
 });
-
 app.get('/views/news.pug', function (req, res) { 
     res.render('news.pug');
 });
-
 app.get('/views/about.pug', function (req, res) { 
     res.render('about.pug');
 });
-
 app.get('/views/services.pug', function (req, res) { 
     res.render('services.pug');
 });
-
 app.get('/views/projects.pug', function (req, res) { 
     res.render('projects.pug');
 });
-
 app.get('/views/contacts.pug', function (req, res) { 
     res.render('contacts.pug');
 });
 
-// BodyParser
+app.get('/views/user/signup.pug', function (req, res) { 
+    res.render('signup.pug');
+});
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// DB config
-const db = require('./config/keys').mongoURI;
+// DB Config
+const db = config.get('mongoURI');
 
-// onnect to Mongo
+// Connect to MongoDB
 mongoose
-    .connect(db)
-    .then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err));
+  .connect(db, { useNewUrlParser: true }) // Removed deprecation warrning
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-// Use routes
-app.use('/api/Users', users);
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 3000
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
